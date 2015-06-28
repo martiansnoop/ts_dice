e2a7d9c4af65357a8e68dad0f9742e432b17a183
Emily Wenberg <martiansnoop@gmail.com>
2015-06-28 15:53:11 -0700

    Expand slightly on stub test for Die

--- test/ts/DieTest.ts
+++ test/ts/DieTest.ts
@@ -1,7 +1,17 @@
 ///<reference path="../../typings/jasmine/jasmine.d.ts"/>
+///<reference path="../../src/ts/Die.ts"/>
 
-describe("test", function() {
-    it("is a test!", function() {
+import Die = dice.Die;
 
+describe("Die", function() {
+    it("rolls a number between 1 and numSides", function() {
+        var sides = 6;
+        var die = new Die(sides);
+        //not deterministic, but not really important for demo purposes
+        for(var i = 0; i < 1000; i++) {
+            var result = die.roll();
+            expect(result).toBeGreaterThan(0);
+            expect(result).toBeLessThan(sides + 1);
+        }
     })
 });
