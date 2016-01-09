b7d12c003f31fcd75e2d39f4c83ba2fffb0999e3
Emily Wenberg <martiansnoop@gmail.com>
2016-01-09 11:47:12 -0800

    Add a way to change the result of the die viewer
    I wanted to make handleClick a public method on the DieRollerView
    class, but for some reason the value of `this` doesn't get set to
    the class instance in the commented-out code. Probably just some
    small thing that I am missing.

--- src/ts/DieRollerView.tsx
+++ src/ts/DieRollerView.tsx
@@ -15,14 +15,25 @@ namespace components {
     export class DieRollerView extends React.Component<DieRollerViewProps, DieRollerViewState> {
         private foo:number;
         private roller:Roller;
+
+        public handleClick:() => void;
+
         constructor(props:DieRollerViewProps) {
             super(props);
             this.foo = 42;
             this.roller = new Roller(new Die(props.sides), props.modifier);
             this.state = { currentResult: this.roller.roll().toString() };
+            this.handleClick = () => {
+                console.log("this happened", this);
+                this.setState({currentResult: this.roller.roll().toString()});
+            }
         }
-        render() {
-            return <div>{this.state.currentResult}</div>;
+        public render() {
+            return <div>{this.state.currentResult} <button onClick={this.handleClick}>Test click</button>  </div>;
         }
+        //public handleClick() {
+        //    console.log("this happened", this);
+        //    this.state.currentResult = "test";
+        //}
     }
 }
