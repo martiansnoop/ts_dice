747c41195dc6dfd758eab6a4719009a9e3534656
Emily Wenberg <martiansnoop@gmail.com>
2016-01-30 15:17:58 -0800

    Reformat webpack config
    Use double quotes, semicolons and four-space indentation
    because I'm a curmudgeon and this helps me feel safe in the
    world.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var webpack = require('webpack')
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/ts/App.tsx',
+  entry: "./src/ts/App.tsx",
   output: {
-    filename: 'build/bundle.js'
+    filename: "build/bundle.js"
   },
   resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
   },
   module: {
     loaders: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
+      { test: /\.tsx?$/, loader: "ts-loader" }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'React Test',
-      template: 'src/index.html',
-      filename: 'build/index.html',
-      inject: 'body'
+      title: "React Test",
+      template: "src/index.html",
+      filename: "build/index.html",
+      inject: "body"
     }),
     new webpack.ProvidePlugin({
       React: "react",
