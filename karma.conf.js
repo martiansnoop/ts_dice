a9b3286015e7a728b577042559ba1fb66b0aedb3
Emily Wenberg <martiansnoop@gmail.com>
2016-02-24 23:10:43 -0800

    Indent a thing with 4 spaces

--- karma.conf.js
+++ karma.conf.js
@@ -2,94 +2,93 @@
 // Generated on Sun Jun 28 2015 15:22:35 GMT-0700 (PDT)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: "./test",
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["mocha"],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: "./test",
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      "ts/**/*.ts",
-      "ts/**/*.tsx"
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ["mocha"],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            "ts/**/*.ts",
+            "ts/**/*.tsx"
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      "ts/**/*.ts": ["webpack"],
-      "ts/**/*.tsx": ["webpack"]
-    },
+        // list of files to exclude
+        exclude: [],
 
 
-    // test results reporter to use
-    // possible values: "dots", "progress"
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["mocha"],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            "ts/**/*.ts": ["webpack"],
+            "ts/**/*.tsx": ["webpack"]
+        },
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: "dots", "progress"
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ["mocha"],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ["Chrome"],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ["Chrome"],
 
-    webpack: {
-      // karma watches the test entry points
-      // (you don't need to specify the entry option)
-      // webpack watches dependencies
 
-      // webpack configuration
-      module: {
-        loaders: [
-          { test: /\.tsx?$/, loader: "ts-loader" }
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+
+        webpack: {
+            // karma watches the test entry points
+            // (you don't need to specify the entry option)
+            // webpack watches dependencies
+
+            // webpack configuration
+            module: {
+                loaders: [
+                    {test: /\.tsx?$/, loader: "ts-loader"}
+                ]
+            }
+        },
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            noInfo: true
+        },
+
+        plugins: [
+            require("karma-webpack"),
+            require("karma-mocha"),
+            require("karma-chrome-launcher"),
+            require("karma-mocha-reporter"),
+            require("karma-spec-reporter")
         ]
-      }
-    },
-
-    webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      // i. e.
-      noInfo: true
-    },
-
-    plugins: [
-      require("karma-webpack"),
-      require("karma-mocha"),
-      require("karma-chrome-launcher"),
-      require("karma-mocha-reporter"),
-      require("karma-spec-reporter")
-    ]
-  })
+    })
 };
